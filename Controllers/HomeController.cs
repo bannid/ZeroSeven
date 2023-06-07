b008e396198d373451cb79e8e95822774eb00136
Banni <bannid@gmail.com>
2023-06-07 17:11:18 +1000

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ZeroSeven.Models;
+
+namespace ZeroSeven.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.title = "Pet list";
+            IList<PetModel> pets = new List<PetModel>();
+            PetModel pet = new PetModel();
+            pet.Name = "something";
+            pets.Add(pet);
+            return View(pets);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/PetModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZeroSeven.Models
+{
+    public class PetModel
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public double Weight { get; set; }
+    }
+}
