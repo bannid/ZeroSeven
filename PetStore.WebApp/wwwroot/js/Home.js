a8755c2781784474d73cfd79f6b9a6160becc110
Banni <bannid@gmail.com>
2023-06-11 10:47:59 +1000

    Some refactor

---
+++ PetStore.WebApp/wwwroot/js/Home.js
@@ -0,0 +1,20 @@
+﻿function onload() {
+    $(function () {
+        $("#dialog").dialog({
+            autoOpen: false
+        });
+    });
+}
+function openModal(id) {
+    window.deleteContext = id;
+    $("#dialog").dialog('open');
+}
+function closeModal() {
+    window.deleteContext = null;
+    $("#dialog").dialog('close');
+}
+function deleteItem() {
+    const elementID = "deleteLink" + window.deleteContext;
+    document.getElementById(elementID).click();
+    closeModal();
+}
